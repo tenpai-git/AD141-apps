2c372343da5a434a647e7cd72127118acb659618
John Baumlin <git@jab.contact>
2023-07-17 22:45:11 +0900

    First half of Collection Exercises

--- collections/practice3-2.py
+++ collections/practice3-2.py
@@ -1,3 +1,19 @@
 #!/bin/python3
 
+prompt = "Enter a number (or the word 'end' to quit) "
+numbers = []
+while True:
+    data = input(prompt)
+    if data == "end":
+        break
+    else:
+        numbers.append(int(data))
+
+print("Your list of numbers is: " + str(numbers))
+
+summation = 0
+for element in numbers:
+    summation += element
+
+print("The sum of your numbers is: " + str(summation))
 
--- collections/practice3-3.py
+++ collections/practice3-3.py
@@ -1,3 +1,21 @@
 #!/bin/python3
 
+prompt = "Enter a number (or the word 'end' to quit): "
+verify = {""}
 
+while True:
+    data = input(prompt)
+    if data == "end":
+        break
+    else:
+        #Check for duplicates before adding into the set. 
+        for element in verify.copy(): #Need to run against a copy!
+            #print("Element: " + element + " Data: " + data)
+            #If the inputted data is not in the set, add it. 
+            if data != element:
+                verify.add(data)
+            #Otherwise, say it is not unique. 
+            else:
+                print("Not a Unique Name")
+verify.remove("");
+print(verify)
