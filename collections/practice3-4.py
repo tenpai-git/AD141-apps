e348b730bf1588f1ff5c35e2c06500068a6a80e4
John Baumlin <git@jab.contact>
2023-07-19 01:17:00 +0900

    List Sorting Exercise

--- collections/practice3-4.py
+++ collections/practice3-4.py
@@ -1,3 +1,23 @@
 #!/bin/python3
 
+prompt = "Enter a name (or the word 'end' to quit): "
+verify = set()
+while True:
+    data = input(prompt)
+    if data == "end":
+        break
+    else:
+        #Check for duplicates before adding into the set. 
+        verify.update(data.split())
 
+print("The set is: " + str(verify)) 
+
+word_list = list()
+
+for element in verify:
+    print(element)
+    word_list.append(element)
+
+word_list.sort()
+print(word_list)
+print("The word list has " + str(len(word_list)) + " unique words total.")
