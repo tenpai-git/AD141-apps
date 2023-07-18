021dff6cc8984cabc1af4f517f550ba0e3f2daa7
John Baumlin <git@jab.contact>
2023-07-19 04:58:07 +0900

    Lists are sorted, still need to implement counter.

--- collections/practice3-5.py
+++ collections/practice3-5.py
@@ -1,3 +1,15 @@
 #!/bin/python3
 
+digit_dict = { "Zero":0,"One":1,"Two":2,"Three":3,"Four":4,"Five":5,"Six":6,"Seven":7,"Eight":8,"Nine":9,"Ten":10 }
 
+prompt = input("Input a set of integers to convert into words.") 
+text = "{}, "
+output = str()
+
+#Reverse lookup dictionary for Key. 
+for integer in prompt:
+    for key, value in digit_dict.items():
+        if int(integer) == int(value):
+            output += text.format(key)
+
+print(output[:-2])
--- collections/practice3-6.py
+++ collections/practice3-6.py
@@ -1,3 +1,25 @@
 #!/bin/python3
 
+prompt = "Enter a name (or the word 'end' to quit): "
+verify = dict()
+i = 0
+while True:
+    data = input(prompt)
+    if data == "end":
+        break
+    else:
+        #Update an element and add to dictionary. 
+        newdict = dict()
+        newdict[i] = data
+        verify.update(newdict)
+        i = i + 1
 
+print("The dictionary is: " + str(verify)) 
+#Sort By Key 
+sorted_dict_key = dict(sorted(verify.items()))
+#Sort By Value, Inverting
+inverted_dict = {v: k for k, v in verify.items()}
+sorted_dict_value = dict(sorted(inverted_dict.items()))
+
+print(sorted_dict_key)
+print(sorted_dict_value)
