fa5acaded0dd03add8f9ad39cd8080cf1f8d1cff
John Baumlin <git@jab.contact>
2023-07-25 02:53:07 +0900

    Exercise 1 Defining a Function

--- functions/practice4-1.py
+++ functions/practice4-1.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+def positive_integer():
+    prompt = int(input("Please enter a positive integer: "))
+    if prompt > 0:
+        return prompt
+    else:
+        return 0
+
+test = positive_integer()
+
+while test == 0: 
+    print("Invalid entry, please try again.")
+    test = positive_integer()
+
+print("Number is positive!")
+
