b7d021d846340970743ef1508de59fced946bb39
John Baumlin <git@jab.contact>
2023-07-27 22:51:41 +0900

    Rewrote sum function using multiple variables referenced.

--- functions/practice4-3.py
+++ functions/practice4-3.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+
+def newSum(*args):
+    total_sum = int()
+    for sum in args:
+        total_sum += sum
+    return total_sum
+
+prompt = str()
+newlist = []
+i = int()
+
+#while True:
+#    if prompt = "End"
+#        break
+#    else
+#        prompt = input("Enter numbers to add (Type \"end\" to end program.): ") 
+#        newlist.append(prompt)
+
+print(newSum(5,3,4,5,6,2))
+print(newSum(24,434,4))
+
