51ac4ce57515ef5e0f440fb4fbe4799590e3aa7f
John Baumlin <git@jab.contact>
2023-07-31 01:19:51 +0900

    Exercise avoid iterating over lists while modifying them. Use copy()

--- functions/practice4-6.py
+++ functions/practice4-6.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def positive(p_list):
+    for item in p_list.copy():
+        if item < 0:
+            p_list.remove(item)
+            print(item)
+            print(p_list)
+    return p_list
+
+parameterList = [-1, -2, 3, -10, 4, 5, -3404, 403]
+print(f"Parameter list {parameterList}")
+print("Returning only positive elements.")
+newList = positive(parameterList)
+print("New List: " + str(newList))
+
+
+
+
+
+
