9a8ffa956472b044448666c1625cf7262cb77ae3
John Baumlin <git@jab.contact>
2023-07-30 22:08:32 +0900

    Exercise 3 modification for Ex 4 - return a tuple of the list.

--- functions/practice4-4.py
+++ functions/practice4-4.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def newSum(*args):
+    newList = []
+    for element in args:
+        newList.append(element)
+    return tuple(newList)
+
+
+print(newSum(5,6,7))
+print(type(newSum(5,6,7)))
