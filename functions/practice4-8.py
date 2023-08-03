72e760f925dbc109a27517f28a307401ce1a0b69
John Baumlin <git@jab.contact>
2023-08-04 03:43:39 +0900

    Nested Function

--- functions/practice4-8.py
+++ functions/practice4-8.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def innerFunction(x,y):
+    return x + y
+
+def outerFunction(a,b):
+    sumNum = innerFunction(a,b)
+    print(sumNum)
+
+g = 5
+m = 10
+
+outerFunction(g,m)
