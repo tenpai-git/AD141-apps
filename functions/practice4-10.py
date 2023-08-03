c621afd85b8546a4c18fc9f86db964a0c1797f25
John Baumlin <git@jab.contact>
2023-08-04 04:22:27 +0900

    Lambda function practice.

--- functions/practice4-10.py
+++ functions/practice4-10.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def outerFunction(a,b):
+    sumNum = (lambda: a + b)()
+    return sumNum
+
+g = 5
+m = 10
+
+result = outerFunction(g,m)
+print(result)
--- functions/practice4-9.py
+++ functions/practice4-9.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def innerFunction(x,y):
+    return x + y
+
+def outerFunction():
+    sumNum = innerFunction(g,m)
+    print(sumNum)
+
+g = 5
+m = 10
+
+outerFunction()
