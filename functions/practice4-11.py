d1504ed48c6e8055ff1b6ef644266ce1677168ac
John Baumlin <git@jab.contact>
2023-08-06 14:49:56 +0900

    Comparing two lists, set comparison also an option.

--- functions/practice4-11.py
+++ functions/practice4-11.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+def commonFunctions(list_one, list_two):
+    new_list = list()
+    for element_one in list_one:
+        for element_two in list_two:
+            if element_one == element_two:
+                new_list.append(element_two)
+    return new_list
+
+abc = [1,2,3,4,5,6,7]
+xyz = [5,6,7,8,9,10]
+
+print(commonFunctions(abc, xyz))
+
