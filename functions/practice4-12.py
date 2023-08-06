293da4a12873c63c07fd29a81488990fb76511bc
John Baumlin <git@jab.contact>
2023-08-06 15:20:57 +0900

    Occurence Finder Script

--- functions/practice4-12.py
+++ functions/practice4-12.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def valueAdd(dictionaryVal, number):
+    new_dict = dict()
+    for key, value in dictionaryVal.items():
+        value += number
+        new_dict[key] = value
+    return new_dict
+
+dictionaryValues = {'a':10, 'b':20, 'c':30, 'd':40, 'e':50}
+adder = 5
+
+print(valueAdd(dictionaryValues, adder))
--- functions/practice4-13.py
+++ functions/practice4-13.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+def commonFunctions(list_one, list_two, occurence_number):
+    count = 0
+    new_list = list()
+    for element_one in list_one:
+        for element_two in list_two:
+            if element_one == element_two:
+                count += 1
+                print(f"Occurence {count} Found!")
+                if count == occurence_number:
+                    print("Desired Occurence Found!")
+                    new_list.append(element_two)
+    return new_list
+
+abc = [1,2,5,5,5,6,7]
+xyz = [5]
+counter = 3
+
+print(commonFunctions(abc, xyz, counter))
+
