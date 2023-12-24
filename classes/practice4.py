57da41b1084ae95494a0afbe9ae6b49fee5cfe93
John Baumlin <git@jab.contact>
2023-12-24 17:08:12 +0900

    Using super().__init__ proper inheritance

--- classes/practice4.py
+++ classes/practice4.py
@@ -13,31 +13,35 @@ class Worker:
     def __repr__(self):
         return self.name
 
-class Manager:
+class Manager(Worker):
 
-    def __init__(self, worker):
-        self.years_worked = worker.years_worked
-        self.salary = worker.salary
+    def __init__(self, name, salary, years_worked):
+        super().__init__(name, salary, years_worked)
+#        self.years_worked = worker.years_worked
+#        self.salary = worker.salary
 
     def pension(self):
         return self.years_worked * (self.salary*0.20)
 
-class Executive:
+class Executive(Worker):
 
-    def __init__(self, worker):
-        self.years_worked = worker.years_worked
-        self.salary = worker.salary
+    def __init__(self, name, salary, years_worked):
+        super().__init__(name, salary, years_worked)
+#        self.years_worked = worker.years_worked
+#        self.salary = worker.salary
 
     def pension(self):
         return self.years_worked * (self.salary*0.30)
 
 def main():
     joe_worker = Worker("Joe", 250000, 5)
-    manager_joe = Manager(joe_worker)
-    executive_joe = Executive(joe_worker)
+    manager_jane = Manager("Jane", 250000, 5)
+    executive_smith = Executive("Smith", 250000, 5)
+#    manager_joe = Manager(joe_worker)
+#    executive_joe = Executive(joe_worker)
     print("Base Pay: " + str(joe_worker.pension()))
-    print("Manager Pay: " + str(manager_joe.pension()))
-    print("Executive Pay: " + str(executive_joe.pension()))
+    print("Manager Pay: " + str(manager_jane.pension()))
+    print("Executive Pay: " + str(executive_smith.pension()))
 
 if __name__ == "__main__":
     main()
