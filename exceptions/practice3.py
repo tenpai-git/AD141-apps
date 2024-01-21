96545ccd3277eb3291eed56721fae65f57190ee5
John Baumlin <git@jab.contact>
2024-01-21 12:00:00 +0900

    End of File and KeyboardInterrupt handling practice.

--- exceptions/practice3.py
+++ exceptions/practice3.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+def main():
+    numArray = []
+
+
+    while True:
+        try: 
+            value = input("Enter an index integer or type \"end\" to exit: ")
+            if value == "end":
+                break
+            else:
+                toIndexNumArray = int(value)
+                numArray.append(toIndexNumArray)
+        except ValueError:
+            print("Please enter an integer instead.")
+        except KeyboardInterrupt:
+            print("\nExiting Program.")
+            exit()
+        except EOFError: 
+            print("\nCaught an End of File Signal, Breaking out of Loop and Exiting")
+            break
+
+    return numArray
+
+if __name__ == "__main__":
+    result = main()
+    print("The final list is: " + str(result))
+    print("The sum of all integers is: " + str(sum(result)))
