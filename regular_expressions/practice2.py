553da88a51ba9093ca2a4c8a88af753d17c0e101
John Baumlin <git@jab.contact>
2024-08-23 16:52:57 +0900

    Floating point regular expression test.

--- regular_expressions/practice2.py
+++ regular_expressions/practice2.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+import re
+
+def main():
+
+    #Read in the data line-by-line
+    with open("2_data.txt", "r") as raw_data:
+        for data_line in raw_data:
+            
+            print("Scanning: ", data_line, end="")
+            
+            #Match any positive or negative number but not any non-digits.
+            regex_float_neg_num = "^(-)+[0-9]+\.[0-9]+[^0-9]*$"
+            regex_float_pos_num = "^[0-9]+\.[0-9]+[^0-9]*$"
+            pos_test = re.search(regex_float_pos_num, data_line)
+            neg_test = re.search(regex_float_neg_num, data_line)
+
+            if pos_test:
+                print("Result: Positive Float", end="\n\n")
+            elif neg_test:
+                print("Result: Negative Float", end="\n\n")
+            else:
+                print("Not a pos/negative float", end="\n\n")
+
+if __name__ == "__main__": 
+    main()
